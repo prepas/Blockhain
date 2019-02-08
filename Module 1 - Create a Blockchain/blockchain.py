e05280d2c4dae5131eb4b2e38a5f694092a65f9e
prepas <prepa@ukr.net>
2019-02-08 09:50:10 +0200

    add is_valid

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -11,13 +11,14 @@ class Blockchain:
     
     def __init__ (self):
         self.chain = []
-        self.create_block(proof = 1, previous_hash = '0')
+        self.create_block(proof = 1, previous_hash = '0', hash_operation = '0')
         
-    def create_block(self, proof, previous_hash):
+    def create_block(self, proof, previous_hash, hash_operation):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
-                 'previous_hash': previous_hash}
+                 'previous_hash': previous_hash,
+                 'hash_operation': hash_operation}
         self.chain.append(block)
         return block
         
@@ -27,13 +28,14 @@ class Blockchain:
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
+        hash_operation = ''
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
-        return new_proof
+        return new_proof, hash_operation
     
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
@@ -68,14 +70,15 @@ blockchain = Blockchain()
 def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
-    proof = blockchain.proof_of_work(previous_proof)
+    proof, hash_operation = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
+    block = blockchain.create_block(proof, previous_hash, hash_operation)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'hash_operation': block['hash_operation']}
     return jsonify(response), 200
 
 #Getting the full Blockchaing
