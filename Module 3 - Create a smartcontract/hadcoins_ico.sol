90e314f5476bfa99051f3be2a0f2175ce8c7f6ab
prepas <prepa@ukr.net>
2019-03-05 09:23:37 +0200

    update hadcoins_ico.sol

--- Module 3 - Create a smartcontract/hadcoins_ico.sol
+++ Module 3 - Create a smartcontract/hadcoins_ico.sol
@@ -17,4 +17,36 @@ contract hadcoins_ico {
     // Mapping from the investor address to its equity in Hadcoins and USD
     mapping(address => uint) equity_hadcoins;
     mapping(address => uint) equity_usd;
+    
+    // Checking if an investor can buy Hadcoins
+    modifier can_buy_hadcoins(uint usd_invested) {
+        require (usd_invested * usd_to_hadcoins + total_hadcoins_bought <= max_hadcoins);
+        _;
+    }
+    
+    // Getting the equity in Hadcoins of investor
+    function equity_in_hadcoins(address investor) external constant returns (uint) {
+        return equity_hadcoins[investor];
+    }
+    
+    // Gettings the equity in USD of an investor
+    function equity_in_usd(address investor) external constant returns (uint) {
+        return equity_usd[investor];
+    }
+    
+    // Buying Hadcoins
+    function buy_hadcoins(address investor, uint usd_invested) external
+    can_buy_hadcoins(usd_invested) {
+        hadcoins_bought = usd_invested * usd_to_hadcoins;
+        equity_hadcoins[investor] += hadcoins_bought;
+        equity_usd[investor] = equity_hadcoins[investor] / 1000;
+        total_hadcoins_bought += hadcoins_bought;
+    }
+    
+    // Selling Hadcoins
+    function sell_hadcoins(address investor, uint hadcoins_sold) external {
+        equity_hadcoins[investor] += hadcoins_sold;
+        equity_usd[investor] = equity_hadcoins[investor] / 1000;
+        total_hadcoins_bought -= hadcoins_sold;
+    }
 }
